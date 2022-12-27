787ef132bd5421a82876ba1b6e75b9f545e81925
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2022-12-26 18:36:16 -0600

    updated pages, components, and app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import {Route , Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Stock from './pages/Stock'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/stocks/:symbol' element={<Stock />} />
+        <Route path='/stocks' element={<Dashboard />} />
+      </Routes>
 
     </div>
   );
